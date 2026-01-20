1d5a0b8d45c2144013b18e89f42fb665a4cdc219
Deon Van vuuren <dee10810@gmail.com>
2026-01-20 22:37:04 +0200

    Ultra-Stable Build: Removed PWA plugin for standard Next.js build

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Force webpack to handle the PWA plugin injection
-  webpack: (config) => {
-    return config;
-  },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
